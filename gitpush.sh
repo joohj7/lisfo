f4d6aae863028c0d25b73e7e2a2adc65af75f140
hj.joo <hj.joo@kt.com>
2023-05-18 12:51:23 +0900

    is uploaded(auto)

--- gitpush.sh
+++ gitpush.sh
@@ -5,7 +5,7 @@
 # Contact: joohj7@gmail.com
 
 
-# REPLACE THIS as your github.io structure
+# REPLACE THIS as your github.io structure 
 web_folder_path='web-data' # 여기서 /을 앞에 넣으면 안됨. 이미지 경로 할때는 앞에 / 넣어줘야 url상에서 image폴더 찾을 수 있지만 로컬에서 실행할때는 루트로 들어가기에...
 
 # Changing a file name and move
