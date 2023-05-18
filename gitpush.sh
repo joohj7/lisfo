00efadb4f6c27beb2bd03ca24d39244a35526b2a
hj.joo <hj.joo@kt.com>
2023-05-18 12:52:32 +0900

    is uploaded(auto)

--- gitpush.sh
+++ gitpush.sh
@@ -6,12 +6,12 @@
 
 
 # REPLACE THIS as your github.io structure 
-web_folder_path='web-data' # 여기서 /을 앞에 넣으면 안됨. 이미지 경로 할때는 앞에 / 넣어줘야 url상에서 image폴더 찾을 수 있지만 로컬에서 실행할때는 루트로 들어가기에...
+web_folder_path='web-data' 
 
 # Changing a file name and move
 # If directories not exist, make it. 
 
 # git add
 git add *
-git commit -m "[$date] $fixed_filename is uploaded(auto)" 
+git commit -m "$fixed_filename is uploaded(auto)" 
 git push 
