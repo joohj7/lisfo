6b572dda87243c7594a402755f61fb59345f2739
hj.joo <hj.joo@kt.com>
2023-05-18 13:26:07 +0900

    file is uploaded(auto)

--- gitpush.sh
+++ gitpush.sh
@@ -4,14 +4,11 @@
 # URL: https://github.com/joohj7/lisfo-global.git 
 # Contact: joohj7@gmail.com
 
-
 # REPLACE THIS as your github.io structure 
 web_folder_path='web-data' 
 
-# Changing a file name and move
-# If directories not exist, make it. 
-
-# git add
+# git add & Push
+# git add $web_folder_path/*
 git add *
 git commit -m "file is uploaded(auto)" 
 git push 
