7546b589160136eb1e3059d4127fa76a138b66ce
hj.joo <hj.joo@kt.com>
2023-05-24 10:54:54 +0900

    file is uploaded(auto)

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // script.js
-
-import  {createFolderElements}  from './utils.js';
-
 document.addEventListener('DOMContentLoaded', function() {
       fetch('folders.json')
         .then(response => response.json())
@@ -16,3 +13,37 @@ document.addEventListener('DOMContentLoaded', function() {
           console.error('Error loading folders.json:', error);
         });
 });
+
+function createFolderElements(foldersData, parentElement, rootFolder) {
+  foldersData.forEach(folderData => {
+    const folderElement = document.createElement('div');
+    folderElement.classList.add('folder');
+
+    const folderNameElement = document.createElement('div');
+    folderNameElement.classList.add('folder-name');
+    folderNameElement.textContent = folderData.id;
+    folderNameElement.addEventListener('click', function() {
+      folderElement.classList.toggle('expanded');
+    });
+    folderElement.appendChild(folderNameElement);
+
+    const files = folderData.files;
+    if (files && files.length > 0) {
+      const fileListElement = document.createElement('ul');
+      fileListElement.classList.add('file-list');
+
+      files.forEach(function(file) {
+        const fileElement = document.createElement('li');
+        const fileLinkElement = document.createElement('a');
+        fileLinkElement.textContent = file.split('.')[0];
+        fileLinkElement.href = `${rootFolder}/${folderData.id}/${encodeURIComponent(file)}`;
+        fileElement.appendChild(fileLinkElement);
+        fileListElement.appendChild(fileElement);
+      });
+
+      folderElement.appendChild(fileListElement);
+    }
+
+    parentElement.appendChild(folderElement);
+  });
+}
