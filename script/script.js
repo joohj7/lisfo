f40aa09162c597ae86c7ba7d29cbeffd49e3bfb1
hj.joo <hj.joo@kt.com>
2023-05-24 13:46:46 +0900

    file is uploaded(auto)

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// script.js
+
+import { createFolderElements, loadHTMLFile } from './utils.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('folders.json')
+    .then(response => response.json())
+    .then(data => {
+      const rootFolder = data.rootFolder;
+      const foldersData = data.folders;
+
+      const sidebarElement = document.querySelector('.sidebar');
+      const mainElement = document.querySelector('.main');
+      createFolderElements(foldersData, sidebarElement, rootFolder, mainElement);
+    })
+    .catch(error => {
+      console.error('Error loading folders.json:', error);
+    });
+});
